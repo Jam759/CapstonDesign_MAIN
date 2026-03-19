61979e97c503976a044cd2ac45d250b1f03f64e8
Jam759 <jemin6372@naver.com>
2026-03-19 21:09:27 +0900

    웹훅 이벤트 전략패턴 적용 및 구현중

--- src/main/java/com/Hoseo/CapstoneDesign/github/service/strategy/impl/InstallationEventStrategy.java
+++ src/main/java/com/Hoseo/CapstoneDesign/github/service/strategy/impl/InstallationEventStrategy.java
@@ -1,13 +1,28 @@
 package com.Hoseo.CapstoneDesign.github.service.strategy.impl;
 
+import com.Hoseo.CapstoneDesign.github.entity.GithubAppInstallations;
+import com.Hoseo.CapstoneDesign.github.entity.InstallationRepository;
+import com.Hoseo.CapstoneDesign.github.exception.GitHubErrorCode;
+import com.Hoseo.CapstoneDesign.github.exception.GitHubException;
+import com.Hoseo.CapstoneDesign.github.factory.GitHubEntityFactory;
+import com.Hoseo.CapstoneDesign.github.service.GitHubAppInstallationService;
+import com.Hoseo.CapstoneDesign.github.service.GithubAppClientService;
+import com.Hoseo.CapstoneDesign.github.service.InstallationRepositoryService;
 import com.Hoseo.CapstoneDesign.github.service.strategy.GithubWebhookStrategy;
 import com.fasterxml.jackson.databind.JsonNode;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-@Slf4j
-@Component
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class InstallationEventStrategy implements GithubWebhookStrategy {
+
+    private final GithubAppClientService githubAppClientService;
+    private final GitHubAppInstallationService gitHubAppInstallationService;
+    private final InstallationRepositoryService installationRepositoryService;
 
     @Override
     public boolean supports(String eventType) {
@@ -16,22 +31,38 @@ public class InstallationEventStrategy implements GithubWebhookStrategy {
 
     @Override
     public void handle(JsonNode payload, String deliveryId) {
-        String action = payload.path("action").asText("");
-        long installationId = payload.path("installation").path("id").asLong();
-        String accountLogin = payload.path("installation").path("account").path("login").asText("");
-        String accountType = payload.path("installation").path("account").path("type").asText("");
-        String senderLogin = payload.path("sender").path("login").asText("");
-        String repositoriesUrl = payload.path("repositories_url").asText("");
+        String action = payload.path("action").asText(null);
+        switch (action) {
+            case "created" -> createdHandle(payload, deliveryId);
+            case "deleted" -> deletedHandle(payload, deliveryId);
+            default -> throw new GitHubException(GitHubErrorCode.GIT_HUB_WEBHOOK_UNSUPPORTED_ERROR);
+        }
+    }
 
-        log.info(
-                "GitHub installation event received. deliveryId={}, action={}, installationId={}, accountLogin={}, accountType={}, senderLogin={}, repositoriesUrl={}",
-                deliveryId,
-                action,
-                installationId,
-                accountLogin,
-                accountType,
-                senderLogin,
-                repositoriesUrl
-        );
+    private void createdHandle(JsonNode payload, String deliveryId) {
+        JsonNode installationNode = payload.path("installation");
+        long installationId = installationNode.path("id").asLong();
+        GithubAppInstallations installation = gitHubAppInstallationService.getByid(installationId);
+        JsonNode repositoriesNode = payload.path("repositories");
+
+        List<InstallationRepository> installationRepositories = new ArrayList<>();
+        if (!repositoriesNode.isArray()) {
+            // repository_selection=all 인 경우 webhook에 전체 목록이 비어있거나 생략된 상황까지 방어
+            // 이거는 client 이용해서 끌어오기
+
+            return;
+        }else {
+            for (JsonNode repoNode : repositoriesNode) {
+                InstallationRepository e =
+                        GitHubEntityFactory.toInstallationRepository(installation, repoNode);
+                installationRepositories.add(e);
+            }
+        }
+
+        installationRepositoryService.saveAll(installationRepositories);
+    }
+
+    private void deletedHandle(JsonNode payload, String deliveryId) {
+
     }
 }
