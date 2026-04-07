785a116b50a94fac7cb41d5e2df8846dc1cac72f
Jam759 <jemin6372@naver.com>
2026-04-07 18:30:20 +0900

    테스트용 시큐리티 설정 수정

--- src/main/java/com/Hoseo/CapstoneDesign/security/config/SecurityConfig.java
+++ src/main/java/com/Hoseo/CapstoneDesign/security/config/SecurityConfig.java
@@ -106,8 +106,10 @@ public class SecurityConfig {
         c.setAllowCredentials(true);
         c.setAllowedOrigins(List.of(
                 "http://127.0.0.1:5500",
-                "http://localhost:5500"
-        ));
+                "http://localhost:5500",
+                "http://127.0.0.1:5173",
+                "http://localhost:5173"
+                ));
         c.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         c.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         c.setExposedHeaders(List.of("Set-Cookie"));
